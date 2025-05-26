9e9a26a96cd06f0048067eba95221095bcf2df9c
rineheaj <joshtrineheart@gmail.com>
2025-05-26 11:55:22 -0500

    Some styling changes...

--- team_images.py
+++ team_images.py
@@ -1,18 +1,13 @@
 from PIL import Image
 import streamlit as st
-
+from pathlib import Path
 
 def show_team_bg(team1: str, team2: str):
     col1, col2 = st.columns(2)
 
     try:
-        img1 = Image.open(
-            f"images\{team1.lower()}.jpg"
-        )
-
-        img2 = Image.open(
-            f"images\{team2.lower()}"
-        )
+        img1 = Image.open(f"images/{team1.lower()}.jpg")
+        img2 = Image.open(f"images/{team2.lower()}.jpg")
     except (FileNotFoundError, Exception) as e:
         st.warning("Background image not found for one or both teams.")
         return
@@ -24,4 +19,18 @@ def show_team_bg(team1: str, team2: str):
     with col2:
         st.image(img2, use_container_width=True, caption=team2)
 
-    
+def validate_paths(team1, team2):
+    team_one_path = Path("images")  / (team1.lower() + ".jpg")
+    if team_one_path.exists():
+        print(f"Path found: {team_one_path}")
+
+    team_two_path = Path("images") / (team2.lower() + ".jpg")
+    if team_two_path.exists():
+        print(f"Path found: {team_two_path}")
+
+if __name__ == "__main__":
+    team1 = "Thunder"
+    team2 = "Timberwolves"
+
+    validate_paths(team1, team2)
+
